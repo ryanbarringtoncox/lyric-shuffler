bd6df5d0b8866f1138790787aa220fa4a4c4020e
Ryan Cox <rbc@Ryans-MacBook-Pro.local>
2014-05-22 16:17:08 -0400

    adds song on song shuffle

--- run.py
+++ run.py
@@ -1,12 +1,23 @@
 #!/bin/bash
+all_lyrics_file="all_lyrics.txt"
 
+# holds unshuffle lyrics
+if [ -f $all_lyrics_file ]; then
+  rm $all_lyrics_file
+fi
+touch $all_lyrics_file
+
+# validate songs dir
 if [ ! -d $1 ]; then
   echo $1 is not a valid dir
   exit
 fi
 
+# loop through *.txt files in songs dir and cat to all_lyrics file
 for f in $1/*.txt
 do
-  #echo found $f
-  ./word-shuffler.py $f
+  cat $f >> $all_lyrics_file
 done
+
+# shuffle up all_lyrics to stdout
+./word-shuffler.py $all_lyrics_file 
